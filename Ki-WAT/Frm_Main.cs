7f45534e4a39c3072ab1eee4d275d30a8a2ec6e9
pospaker <pospaker@naver.com>
2025-05-09 15:49:52 +0900

    First

---
+++ Ki-WAT/Frm_Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ki_WAT
+{
+    public partial class Frm_Main : Form
+    {
+        Frm_Mainfrm m_frmParent;
+
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+        public void SetParent(Frm_Mainfrm f)
+        {
+            m_frmParent = f;
+        }
+    }
+}
