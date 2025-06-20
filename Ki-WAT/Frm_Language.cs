01e5f3c068a443027e29703d61b64e82e8d96f4c
pospaker <pospaker@naver.com>
2025-06-20 13:21:42 +0900

    asdf

---
+++ Ki-WAT/Frm_Language.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ki_WAT
+{
+    public partial class Frm_Language : Form
+    {
+        public Frm_Language()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+
+    }
+}
